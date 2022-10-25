5e64b1534274ec1f1b85acf2b43f6cf181fed240
Creeper120-DCUStudios <boyspockie@gmail.com>
2022-10-25 12:26:44 -0400

    random bogus go

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 
 // Make a request for a user with a given ID
 axios.get('https://www.thebluealliance.com/api/v3/team/frc3461?X-TBA-Auth-Key=hOngAA1OYRpYtrfePLodT2G27R7fSBfrGJ57RwH1sOZJxaJv3rHGfhH37aLitVNb')
