2ac715c799c7bf37ca9e4fe1de8b96d157e66878
Creeper120-DCUStudios <boyspockie@gmail.com>
2022-10-25 12:48:40 -0400

    configurable TBA?

--- assets/js/tba.js
+++ assets/js/tba.js
@@ -1,7 +1,8 @@
-// const axios = require('axios');
+---
+---
 
 // Make a request for a user with a given ID
-axios.get('https://www.thebluealliance.com/api/v3/team/frc3461?X-TBA-Auth-Key=hOngAA1OYRpYtrfePLodT2G27R7fSBfrGJ57RwH1sOZJxaJv3rHGfhH37aLitVNb')
+axios.get('https://www.thebluealliance.com/api/v3/{{ page.endpoint }}?X-TBA-Auth-Key=hOngAA1OYRpYtrfePLodT2G27R7fSBfrGJ57RwH1sOZJxaJv3rHGfhH37aLitVNb')
   .then(function (response) {
     // handle success
     console.log(response);
