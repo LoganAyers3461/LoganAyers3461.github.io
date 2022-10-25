800e5cd86ce3709ede36a94f05d20380af660733
Creeper120-DCUStudios <boyspockie@gmail.com>
2022-10-25 12:12:46 -0400

    JS doing JS Things I dont understand

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
+import { get } from 'axios';
 
 // Make a request for a user with a given ID
-axios.get('https://www.thebluealliance.com/api/v3/team/frc3461?X-TBA-Auth-Key=hOngAA1OYRpYtrfePLodT2G27R7fSBfrGJ57RwH1sOZJxaJv3rHGfhH37aLitVNb')
+get('https://www.thebluealliance.com/api/v3/team/frc3461?X-TBA-Auth-Key=hOngAA1OYRpYtrfePLodT2G27R7fSBfrGJ57RwH1sOZJxaJv3rHGfhH37aLitVNb')
   .then(function (response) {
     // handle success
     console.log(response);
